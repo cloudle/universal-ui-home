6bb701654da7dca632de6a7214b3a47ddd02cc8a
cloudle <lehaoson@gmail.com>
2017-06-22 22:20:09 +0700

    Google cloud deployment configs..

--- .next/dist/store/actions.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", { value: true });var IncreaseCounter = exports.IncreaseCounter = '@APP:INCREASE-COUNTER';
-var ToggleRouteLoaded = exports.ToggleRouteLoaded = '@APP:TOGGLE-ROUTE-LOADED';
-var UpdateLoadingProgress = exports.UpdateLoadingProgress = '@APP:UPDATE-LOADING-PROGRESS';
--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const moduleAlias = require('module-alias');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const server = express();
 
 moduleAlias.addAlias('react-native', 'react-native-web');
 
 app.prepare()
 	.then(() => {
-		const server = express();
-
 		// Temporary fix for Bodymovin.. on server side
 		global.requestAnimationFrame = () => {};
 		global.document = {
@@ -28,12 +27,16 @@ app.prepare()
 			return handle(req, res);
 		});
 
-		server.listen(3000, (err) => {
-			if (err) throw err;
-			console.log('> Ready on http://localhost:3000');
-		});
+		if (module === require.main) {
+			server.listen(3000, (err) => {
+				if (err) throw err;
+				console.log('> Ready on http://localhost:3000');
+			});
+		}
 	})
 	.catch((ex) => {
 		console.error(ex.stack);
 		process.exit(1);
 	});
+
+module.exports = server;
